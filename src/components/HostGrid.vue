8305142a4dd1851a9fffdc9907e0dbbcca244191
Ross Weijer <ross@datascopeanalytics.com>
2020-03-18 18:22:12 -0500

    MVP random selection functionality accomplished

--- src/components/HostGrid.vue
+++ src/components/HostGrid.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
     <h1>Who's gonna host MLM on {{ nextHostDate }}?!?!</h1>
-    <h2>Showing {{ numDisplay }} of {{ hostCount }} candidates </h2>
-    <span v-if=candidateSelected> WE GOT'EM!!!!</span>
+    <div v-if=candidateSelected>
+      <div class=modal-mask>
+        <div class=modal-container>
+          <div class=modal-header>
+            Ladies and gentlemen, the Next Host of ChIDEO MLM is...
+          </div>
+          <div class=modal-body>
+            {{ candidateSelected.name }} !!!
+          </div>
+          <div class=modal-footer>
+            <button id=submit-host-button @click="submitHostForMLM()">Put 'em in the books!</button>
+            <button id=pick-another-button @click="chooseHostForMLM()">Pick someone else!</button>
+          </div>
+        </div>
+      </div>
+    </div>
     <table>
       <tr v-for="row in tableRows" :key="row">
         <td v-for="col in tableCols" :key="getDisplayIndex(row, col)" style="{ width: tdWidth }">
@@ -27,9 +41,9 @@ export default {
   data: function () {
     return {
       hostCount: 0,
-      hostList: null,
+      hostList: [],
       nextHostDate: '',
-      sampleHosts: null,
+      sampleHosts: [],
       candidateSelected: null
     }
   },
@@ -69,7 +83,19 @@ export default {
     chooseHostForMLM: function () {
       const choiceIndex = Math.floor(Math.random() * this.hostList.length)
       this.candidateSelected = this.hostList[choiceIndex]
-      alert(`I CHOOSE YOU ${this.candidateSelected.name}!!!`)
+    },
+    submitHostForMLM: function () {
+      const postData = {
+        WeekOfHosting: this.nextHostDate,
+        hostID: this.candidateSelected.id,
+        hostName: this.candidateSelected.name
+      }
+      console.log(JSON.stringify(postData))
+      this.exitModal()
+    },
+    exitModal: function () {
+      this.sampleHosts = this.sampleCandidates(this.hostList)
+      this.candidateSelected = null
     }
   },
   computed: {
@@ -91,12 +117,12 @@ export default {
 <style scoped>
 h1 {
   text-align: left;
-  font-family: 'Comic Sans MS';
+  font-family: Comic Sans MS;
 }
 
 h2 {
   text-align: left;
-  font-family: 'Comic Sans MS';
+  font-family: Comic Sans MS;
 }
 
 table {
@@ -105,19 +131,69 @@ table {
 
 td {
   list-style-type: none;
-  font-family: 'Comic Sans MS';
+  font-family: Comic Sans MS;
   text-align: center;
   padding: 10px;
+}
+
+button {
+  font-family: Comic Sans MS;
+  color: white;
+  border-style: outset;
+  border-radius: 8px;
 }
 
 #choose-host-button {
   display: inline-block;
   background-color: #be03fc;
   min-height: 50px;
-  font-family: 'Comic Sans MS';
-  color: white;
-  border-style: outset;
-  border-radius: 8px;
   margin-top: 2em;
+}
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+/*.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}*/
+
+.modal-container {
+  width: 300px;
+  margin: 50px auto;
+  padding: 33px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Comic Sans MS;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+#pick-another-button {
+  float: right;
+  background-color: #C71585;
+}
+
+#submit-host-button {
+  float: left;
+  background-color: #42b983;
 }
 </style>
